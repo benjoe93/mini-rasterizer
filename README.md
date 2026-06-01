97b8950d6157bd77577c418f9700438aa9f079a5
benjoe93 <blazerbence@gmail.com>
2026-06-01 14:15:05 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mini-rasterizer
 Software rasterizer learning project
+
+# Third-party libraries
+- minifb: [GitHub](https://github.com/emoon/minifb)
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+#define WIDTH 800
+#define HEIGHT 600
+
+#define OUT_PATH = "../../save/out.ppm"
+
+int main() {
+    printf("Hello world!\n");
+    return 0;
+}
